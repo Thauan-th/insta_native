38a8731ece612f0085440a0c10f24c9fe62f5fc1
Thauan <83513806+Thauan-th@users.noreply.github.com>
2022-08-03 16:25:19 -0300

    add: deps for navigation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native'
-
-import Feed from './views/Feed/Feed';
 import Navigator from './Navigator/Navigator'
 
 export default function src() {
  return (
-  //  <NavigationContainer>
     <Navigator/>
-  //  </NavigationContainer>
   );
 }
